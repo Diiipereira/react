56a5c16d3f9937a0ea79f9ca4ad65c4a4e7db589
Diiipereira <dipereira@live.com>
2021-06-10 02:00:58 -0300

    Configuração do eslint, prettier, editorconfig e prop-types

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -8,7 +8,6 @@ import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
 
 export const Home = () => {
-
   const [posts, setPosts] = useState([]);
   const [allPosts, setAllPosts] = useState([]);
   const [page, setPage] = useState(0);
@@ -17,68 +16,52 @@ export const Home = () => {
 
   const noMorePosts = page + postsPerPage >= allPosts.length;
 
-  const filteredPosts = !!searchValue ? 
-  allPosts.filter(post => { 
-    return post.title.toLowerCase().includes(
-      searchValue.toLowerCase()
-  );
-  }) 
-  : 
-  posts;
+  const filteredPosts = searchValue
+    ? allPosts.filter((post) => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase());
+      })
+    : posts;
 
-    const handleLoadPosts = useCallback(async (page, postsPerPage) => { 
-      const postsAndPhotos = await loadPosts();
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+    const postsAndPhotos = await loadPosts();
 
-      setPosts(postsAndPhotos.slice(page, postsPerPage));
-      setAllPosts(postsAndPhotos);
-    }, [])
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
 
-    useEffect(() => {
-      handleLoadPosts(0, postsPerPage);
-    }, [handleLoadPosts, postsPerPage]);
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
 
-    const loadMorePosts = () => { 
-      const nextPage = page + postsPerPage;
-      const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-      posts.push(...nextPosts);
+  const loadMorePosts = () => {
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
 
-      setPosts(posts);
-      setPage(nextPage);
-    }
+    setPosts(posts);
+    setPage(nextPage);
+  };
 
-    const handleChange = (e) => { 
-      const { value } = e.target;
-      setSearchValue(value);
-    }
-  
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  };
+
   return (
     <section className="container">
       <div className="search-container">
-      {!!searchValue && (
-        <h1>Search Value: {searchValue}</h1>
-      )}
+        {!!searchValue && <h1>Search Value: {searchValue}</h1>}
 
-        <TextInput searchValue={searchValue} handleChange={handleChange}
-        />
+        <TextInput searchValue={searchValue} handleChange={handleChange} />
       </div>
-      
-      {filteredPosts.length > 0 &&( 
-      <Posts posts={filteredPosts} />
-      )}
 
-      {filteredPosts.length === 0 &&( 
-      <p className="msg-box">Nenhum post encontrado..</p>
-      )}
+      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+
+      {filteredPosts.length === 0 && <p className="msg-box">Nenhum post encontrado..</p>}
 
       <div className="button-container">
-        {!searchValue && (
-          <Button 
-            text="Carregar Mais Publicações"
-            onClick={loadMorePosts}
-            disabled={noMorePosts}
-          />
-        )}
+        {!searchValue && <Button text="Carregar Mais Publicações" onClick={loadMorePosts} disabled={noMorePosts} />}
       </div>
     </section>
   );
-}
+};
